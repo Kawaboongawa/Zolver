4fb51d57cd77bb58c9263084d42f0e1c339ed84a
Denis <denis.casteran@gmail.com>
2017-11-21 17:09:15 +0100

    extractor: can now find pieces in puzzle with incorrect quality

--- src/Puzzle/Extractor.py
+++ src/Puzzle/Extractor.py
@@ -1,5 +1,5 @@
 from cv2 import cv2
-
+import sys
 import numpy as np
 from Img.filters import *
 
@@ -24,11 +24,40 @@ class Extractor():
         ret, self.img_bw = cv2.threshold(self.img_bw, 240, 255, cv2.THRESH_BINARY_INV)
 
         cv2.imwrite("/tmp/binarized_treshold.png", self.img_bw)
+
+
+
+        def fill_holes():
+            # filling contours found (and thus potentially holes in pieces)
+            _, contour, _ = cv2.findContours(self.img_bw, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+            for cnt in contour:
+                cv2.drawContours(self.img_bw, [cnt], 0, 255, -1)
+            # Inversing colors : start of filling pieces
+            self.img_bw = cv2.bitwise_not(self.img_bw)
+            # filling holes in the pieces 2
+            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
+            self.img_bw = cv2.morphologyEx(self.img_bw, cv2.MORPH_OPEN, kernel)
+            # Inversing colors back : end of filling pieces
+            self.img_bw = cv2.bitwise_not(self.img_bw)
+
+        cv2.imwrite("/tmp/binarized_treshold_filled.png", self.img_bw)
         if self.pixmapWidget is not None:
             self.pixmapWidget.add_image_widget("/tmp/binarized_treshold.png", 1, 1)
 
-        self.img_bw, contours, hier = cv2.findContours(self.img_bw, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        puzzle_pieces = export_contours(self.img, self.img_bw, contours, "/tmp/contours.png", 5)
+        # In case with fail to find the pieces, we fill some holes and then try again
+        nb_error_max = 42
+        while True:
+            try:
+                self.img_bw, contours, hier = cv2.findContours(self.img_bw, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+                puzzle_pieces = export_contours(self.img, self.img_bw, contours, "/tmp/contours.png", 5)
+                break
+            except (ValueError,IndexError):
+                fill_holes()
+                nb_error_max -= 1
+                if nb_error_max <= 0:
+                    print('Could not find the pieces, exiting the app')
+                    sys.exit(1)
+                print('Error while trying to find the pieces, trying again after filling some holes')
         if self.pixmapWidget is not None:
             self.pixmapWidget.add_image_widget("/tmp/contours.png", 0, 1)
 
