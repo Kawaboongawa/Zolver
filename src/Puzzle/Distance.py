08c0ca1d1b3d01e578124fc0e8e9b3ba98091e59
Hugo Rybinski <hugo.rybinski@epita.fr>
2017-11-22 14:36:56 +0100

    comparaison: revert comparaison update

--- src/Puzzle/Distance.py
+++ src/Puzzle/Distance.py
@@ -13,10 +13,17 @@ def normalize_vect_len(e1, e2):
 
 # Match edges by performing a simple norm on each points
 def diff_match_edges(e1, e2):
-    if len(e2) < len(e1) * 0.9 or len(e2) > len(e1) * 1.1:
-        return float('inf')
-    shortest, longest = normalize_vect_len(e1, e2)
     diff = 0
-    for i, p in enumerate(shortest):
-        diff += np.linalg.norm(p[0] - longest[len(longest) - i - 1][0]) ** 2
-    return math.sqrt(diff / len(shortest))  # RMSE
+    for i, p in enumerate(e1):
+        if i < len(e2):
+            diff += np.linalg.norm(p[0] - e2[len(e2) - i - 1][0])
+        else:
+            break
+    return diff
+    # if len(e2) < len(e1) * 0.9 or len(e2) > len(e1) * 1.1:
+    #     return float('inf')
+    # shortest, longest = normalize_vect_len(e1, e2)
+    # diff = 0
+    # for i, p in enumerate(shortest):
+    #     diff += np.linalg.norm(p[0] - longest[len(longest) - i - 1][0]) ** 2
+    # return math.sqrt(diff / len(shortest))  # RMSE
