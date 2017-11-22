73fd8ed5214c23a9f3893528f0f0f59f7cdd1c21
Denis <denis.casteran@gmail.com>
2017-11-22 19:14:25 +0100

    tests: now give an estimation of OK/KO

--- tests/script.py
+++ tests/script.py
@@ -1,7 +1,9 @@
 import subprocess
 import cv2
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import sys
+import os
 
 if len(sys.argv) < 2:
     images=[('resources/parpaing6.png', 'resources/parpaing6.pngRef')]
@@ -14,18 +16,29 @@ for img, ref in images:
   print('Trimming output')
   subprocess.run(["convert", "-trim", "/tmp/colored.png", "tmp.png"])
   print('Diff between solved puzzle and ref')
-  subprocess.run(["compare", "-metric", "ae", ref, "tmp.png", "null:"])
+  out = subprocess.run(["compare", "-metric", "ae", ref, "tmp.png", "null:"], stderr=subprocess.PIPE)
+
+  diff = int(out.stderr)
+  cvImage = cv2.imread(img)
+  w, h, channels = cvImage.shape
+  ratio = (diff / (w * h))
+  print('ratio:', ratio)
+  if (ratio > 0.001):
+    print('Estimation : FAILED')
+  else:
+    print('Estimation : OK')
+
 
   fig = plt.figure("Images")
 
   ax = fig.add_subplot(1, 2, 1)
   ax.set_title("Ref image")
-  plt.imshow(cv2.imread(ref))
+  plt.imshow(mpimg.imread(ref))
   plt.axis("off")
 
   ax = fig.add_subplot(1, 2, 2)
   ax.set_title("Reconstitution")
-  plt.imshow(cv2.imread("tmp.png"))
+  plt.imshow(mpimg.imread("tmp.png"))
   plt.axis("off")
   plt.show()
 
