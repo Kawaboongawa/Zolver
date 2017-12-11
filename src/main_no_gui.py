146db60ede8e7eca20c94ebf7f634189ab7315b4
Hugo Rybinski <hugo.rybinski@epita.fr>
2017-12-11 18:18:17 +0100

    main: fix hardcoded path

--- src/main_no_gui.py
+++ src/main_no_gui.py
@@ -6,11 +6,5 @@ fig_size = plt.rcParams["figure.figsize"]
 fig_size[0] = fig_size[0] * 2
 plt.rcParams["figure.figsize"] = fig_size
 
-# Puzzle(sys.argv[1])
-path = '/home/hugo/ing3/pfe/zolver/resources/'
-Puzzle(path + 'tomatoes.png')
-# Puzzle(path + 'moogly.png')
-# Puzzle(path + 'parpaing6.png')
-# Puzzle(path + 'statue.png')
-# Puzzle(path + 'colorfull.png')
-# Puzzle(path + 'quad_puzzle.png')
+Puzzle(sys.argv[1])
+
