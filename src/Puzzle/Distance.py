76cf0562d831f314b2e6007dc202d5f935a38575
Jérémy Lugand <lugand_j@epita.fr>
2017-12-13 14:39:31 +0100

    add: Add final color Lab evaluation function

--- src/Puzzle/Distance.py
+++ src/Puzzle/Distance.py
@@ -38,32 +38,40 @@ def diff_match_edges(e1, e2, reverse=True):
 
 
 # Match edges by performing a simple norm on each points
-def old_diff_match_edges(e1, e2, reverse=True, thres=5):
+def old_diff_match_edges(e1, e2, reverse=True, thres=5, pad=False):
     if e2.shape[0] > e1.shape[0]:
         e1, e2 = e2, e1
     
-    pad_length = (e1.shape[0] - e2.shape[0]) // 2
-    pad_left, pad_right = pad_length, (pad_length if pad_length * 2 == (e1.shape[0] - e2.shape[0]) else pad_length + 1)
+    if pad:
+        pad_length = (e1.shape[0] - e2.shape[0]) // 2
+        pad_left, pad_right = pad_length, (pad_length if pad_length * 2 == (e1.shape[0] - e2.shape[0]) else pad_length + 1)
 
-    # Pad the shortest with 0
-    e2 = np.lib.pad(e2, ((pad_left, pad_right), (0, 0)), 'constant', constant_values=(0, 0))
+        # Pad the shortest with 0
+        e2 = np.lib.pad(e2, ((pad_left, pad_right), (0, 0)), 'constant', constant_values=(0, 0))
+    else:
+        # No padding just cut longest to match shortest length
+        e1 = e1[:e2.shape[0]]
 
     if reverse:
         e2 = np.flip(e2, 0)
     d = np.linalg.norm(e1 - e2, axis=1)
     return np.sum(d > thres) / e1.shape[0]
 
-def diff_colors(e1_lab_colors, e2_lab_colors, reverse=False, JNC=2.3):
+def diff_colors(e1_lab_colors, e2_lab_colors, reverse=True, JNC=2.3, pad=False):
     #print(e1_lab_colors.shape[0], e2_lab_colors.shape[0])
     # Swap values to have longer first
     if e2_lab_colors.shape[0] > e1_lab_colors.shape[0]:
         e1_lab_colors, e2_lab_colors = e2_lab_colors, e1_lab_colors
     
-    pad_length = (e1_lab_colors.shape[0] - e2_lab_colors.shape[0]) // 2
-    pad_left, pad_right = pad_length, (pad_length if pad_length * 2 == (e1_lab_colors.shape[0] - e2_lab_colors.shape[0]) else pad_length + 1)
+    if pad:
+        pad_length = (e1_lab_colors.shape[0] - e2_lab_colors.shape[0]) // 2
+        pad_left, pad_right = pad_length, (pad_length if pad_length * 2 == (e1_lab_colors.shape[0] - e2_lab_colors.shape[0]) else pad_length + 1)
 
-    # Pad the shortest with 0
-    e2_lab_colors = np.lib.pad(e2_lab_colors, ((pad_left, pad_right), (0, 0)), 'constant', constant_values=(0, 0))
+        # Pad the shortest with 0
+        e2_lab_colors = np.lib.pad(e2_lab_colors, ((pad_left, pad_right), (0, 0)), 'constant', constant_values=(0, 0))
+    else:
+        # No padding just cut longest to match shortest length
+        e1_lab_colors = e1_lab_colors[:e2_lab_colors.shape[0]]
 
     if reverse:
         e2_lab_colors = np.flip(e2_lab_colors, 0)
@@ -84,10 +92,10 @@ def diff_full_compute(e1, e2):
         rgb = hls_to_rgb(col[0], col[1], col[2])
         e2_lab_colors.append(color.rgb2lab([[[rgb[0] / 255.0, rgb[1] / 255.0, rgb[2] / 255.0]]])[0][0])
         # Drop Luminance
-        e1_lab_colors[-1] = [0, e1_lab_colors[-1][1], e1_lab_colors[-1][2]]
+        e2_lab_colors[-1] = [0, e2_lab_colors[-1][1], e2_lab_colors[-1][2]]
 
     edge_shape_score = old_diff_match_edges(np.array(e1.shape), np.array(e2.shape))
-    edge_color_score = diff_colors(np.array(e1_lab_colors), np.array(e2_lab_colors), reverse=True, JNC=10)
+    edge_color_score = diff_colors(np.array(e1_lab_colors), np.array(e2_lab_colors))
 
     # Sigmoid
     L = 10
@@ -95,8 +103,8 @@ def diff_full_compute(e1, e2):
     #edge_color_score = 1 / (1 + math.exp(-L * (edge_color_score - 0.5)))
     edge_shape_score = (K * edge_shape_score) / (K - edge_shape_score + 1)
 
-    #print(e1.type, e2.type, edge_color_score, edge_shape_score, (edge_color_score + edge_shape_score) / 2)
-    #return edge_shape_score
+    print(e1.type, e2.type, edge_color_score, edge_shape_score, (edge_color_score + edge_shape_score) / 2)
+    return edge_color_score
     return (edge_color_score + edge_shape_score) / 2
 
 
